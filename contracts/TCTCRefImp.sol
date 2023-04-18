7df1d1c68c7971b70c062c6536b667874b406f0a
Ko Fujimura <fujimura.ko@gmail.com>
2023-04-18 13:33:28 +0900

    Update TCTCRefImp.sol

--- contracts/TCTCRefImp.sol
+++ contracts/TCTCRefImp.sol
@@ -5,16 +5,20 @@ pragma solidity ^0.8.9;
 
 import "./TCTC.sol";
 
-contract MyToken is TCTC {
+contract TCTCRefImpl is TCTC {
     event ErcRefImplDeploy(uint256 version, string name, string url);
+
     constructor (uint256 _version) TCTC(
-        "MyToken",
-        "MTK",
-        // See the contract (ERC5679RefImpl.sol) deployed in the Goerli Testnet
-        0x8965B739DF91eB621D9FF06af4A48198f711BbD9, // ContractID of the token minter must have. Need to be modfied
-        0x8965B739DF91eB621D9FF06af4A48198f711BbD9, // ContractID of the token holder must have. Need to be modfied
-        0x8965B739DF91eB621D9FF06af4A48198f711BbD9  // ContractID of the token burner must have. Need to be modfied
+        "TCTCRefImpl",
+        "TCT"
     ) {
-        emit ErcRefImplDeploy(_version, "MyToken", "<tbd>");
+        // ContractID of the token minter must have. Need to be modfied
+        _addControlToken(Role.MINTER, 0x8965B739DF91eB621D9FF06af4A48198f711BbD9);
+        // Comment out the following line so that the condition doesn't apply:
+        // _addControlToken(Role.HOLDER, 0x8965B739DF91eB621D9FF06af4A48198f711BbD9);
+        // ContractID of the token burner must have. Need to be modfied
+        _addControlToken(Role.BURNER, 0x8965B739DF91eB621D9FF06af4A48198f711BbD9);
+ 
+        emit ErcRefImplDeploy(_version, "TCTCRefImpl", "https://github.com/kofujimura/TCTC");
     }
 }
