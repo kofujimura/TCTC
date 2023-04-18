1290dbfaba31194b811e5e60b2c140e2389fb4cd
Ko Fujimura <fujimura.ko@gmail.com>
2023-04-18 13:32:32 +0900

    Update TCTC.sol

--- contracts/TCTC.sol
+++ contracts/TCTC.sol
@@ -1,49 +1,36 @@
 // SPDX-License-Identifier: Apache-2.0
 // Author: Ko Fujimura <ko@fujimura.com>
-// Open source repo: <tbd>
+// Open source repo: https://github.com/kofujimura/TCTC
 
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ERC5679.sol";
 
-abstract contract TCTC is ERC5679Ext721, ERC721 {
-    address private _minterTokenContract;
-    address private _holderTokenContract;
-    address private _burnerTokenContract;
+contract TCTC is ERC5679Ext721, ERC721 {
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address minterTokenContract,
-        address holderTokenContract,
-        address burnerTokenContract
-    ) ERC721(_name, _symbol) {
-        _minterTokenContract = minterTokenContract;
-        _holderTokenContract = holderTokenContract;
-        _burnerTokenContract = burnerTokenContract;
-    }
+    enum Role {MINTER, HOLDER, BURNER}
+
+    mapping (Role => address []) controlTokens;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
     
-    modifier onlyMinter(address u) {
-        require(IERC721(_minterTokenContract).balanceOf(u) >= 1, "TCTC: not minter");
+    modifier onlyHasToken(Role r, address u) {
+        for (uint i = 0; i < controlTokens[r].length; i++) {
+            require(IERC721(controlTokens[r][i]).balanceOf(u) > 0, "TCTC: not own a required token");
+        }
         _;
     }
 
-    modifier onlyHolder(address u) {
-        require(IERC721(_holderTokenContract).balanceOf(u) >= 1, "TCTC: not holder");
-        _;
+    function _addControlToken (Role r, address c) internal {
+        controlTokens[r].push(c);
     }
-
-    modifier onlyBurner(address u) {
-        require(IERC721(_burnerTokenContract).balanceOf(u) >= 1, "TCTC: not burner");
-        _;
-    }    
 
     function safeMint(
         address _to,
         uint256 _id,
         bytes calldata // _data (unused)
-    ) public virtual onlyMinter(_msgSender()) onlyHolder(_to) {
+    ) external virtual override onlyHasToken(Role.MINTER,_msgSender()) onlyHasToken(Role.HOLDER, _to) {
         _safeMint(_to, _id); // ignoring _data in this simple reference implementation.
     }
 
@@ -54,10 +41,9 @@ abstract contract TCTC is ERC5679Ext721, ERC721 {
         address _from,
         address _to,
         uint256 _id
-    ) public virtual override onlyHolder(_from) onlyHolder(_to){
+    ) public virtual override onlyHasToken(Role.HOLDER, _from) onlyHasToken(Role.HOLDER, _to){
         //solhint-disable-next-line max-line-length
         require(_isApprovedOrOwner(_msgSender(), _id), "TCTC: caller is not token owner or approved");
-
         _transfer(_from, _to, _id);
     }
 
@@ -68,9 +54,10 @@ abstract contract TCTC is ERC5679Ext721, ERC721 {
         address _from,
         address _to,
         uint256 _id
-    ) public virtual override {
-        safeTransferFrom(_from, _to, _id, "");
-    }    
+    ) public virtual override onlyHasToken(Role.HOLDER, _from) onlyHasToken(Role.HOLDER, _to) {
+        require(_isApprovedOrOwner(_msgSender(), _id), "TCTC: caller is not token owner or approved");
+        _safeTransfer(_from, _to, _id, "");
+    }
 
      /**
      * @dev See {IERC721-safeTransferFrom}.
@@ -80,7 +67,7 @@ abstract contract TCTC is ERC5679Ext721, ERC721 {
         address _to,
         uint256 _id,
         bytes memory data
-    ) public virtual override onlyHolder(_from) onlyHolder(_to) {
+    ) public virtual override onlyHasToken(Role.HOLDER, _from) onlyHasToken(Role.HOLDER, _to) {
         require(_isApprovedOrOwner(_msgSender(), _id), "TCTC: caller is not token owner or approved");
         _safeTransfer(_from, _to, _id, data);
     }
@@ -89,7 +76,7 @@ abstract contract TCTC is ERC5679Ext721, ERC721 {
         address _from,
         uint256 _id,
         bytes calldata // _data (unused)
-    ) public virtual  onlyBurner(_msgSender()) onlyHolder(_from) {
+    ) external virtual override onlyHasToken(Role.HOLDER, _msgSender()) onlyHasToken(Role.HOLDER, _from) {
         // Depends on applicaton whether
         // require(_isApprovedOrOwner(_msgSender(), _id), "TCTC: caller is not token owner or approved");
         _burn(_id); // ignoring _data in this simple reference implementation.
@@ -98,8 +85,10 @@ abstract contract TCTC is ERC5679Ext721, ERC721 {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC5679Ext721, ERC721)
+        override(ERC721, ERC5679Ext721)
         returns (bool)
-    {}
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
 }
