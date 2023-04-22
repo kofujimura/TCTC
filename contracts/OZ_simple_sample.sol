92fefed912ded5812541bc76f869ce73c39cecab
Ko Fujimura <fujimura.ko@gmail.com>
2023-04-22 09:32:19 +0900

    Update oz path

--- contracts/OZ_simple_sample.sol
+++ contracts/OZ_simple_sample.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.8.3/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.8.3/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./TokenController.sol";
 
 contract MyToken is ERC721, TokenController {
