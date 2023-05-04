73a30892a1984df38ff8cc3e5ac1e47e0a66d55f
Ko Fujimura <fujimura.ko@gmail.com>
2023-05-05 07:11:56 +0900

    Update TokenController.sol

--- contracts/TokenController.sol
+++ contracts/TokenController.sol
@@ -7,23 +7,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenController {
-    mapping (bytes32 => address []) controlTokens;
+    mapping (bytes32 => address []) private _controlTokens;
 
     modifier onlyHasToken(bytes32 r, address u) {
         require(_checkHasToken(r,u), "TokenController: not has a required token");
         _;
     }
 
-    // Specifies a token that must be owned by a user with the specified Role. 
-    // Multiple calls are allowed, in this case the user must own at least one of 
-    // the specified token.
-    function _grantRoleToken (bytes32 r, address c) internal {
-        controlTokens[r].push(c);
+    // Specifies the contract ID of the token that must be owned by a user with the 
+    // specified role. Multiple calls are allowed, in this case the user must own at 
+    // least one of the specified token.
+    function _grantRoleByToken (bytes32 r, address c) internal {
+        _controlTokens[r].push(c);
     }
 
     function _checkHasToken (bytes32 r, address u) internal view returns (bool) {
-        for (uint i = 0; i < controlTokens[r].length; i++) {
-            if (IERC721(controlTokens[r][i]).balanceOf(u) > 0) return true;
+        for (uint i = 0; i < _controlTokens[r].length; i++) {
+            if (IERC721(_controlTokens[r][i]).balanceOf(u) > 0) return true;
         }
         return false;
     }
